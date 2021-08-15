5203733c340c53a5a0c8403ccb71f63a13a4dedd
mohiandipta <mohianmostofa@gmail.com>
2021-08-15 19:47:01 +0600

    class based components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, { Component } from 'react';
 import { name, age, person } from './data';
 import Banner from './components/Header/Banner';
 
-const App = () => (<section>
-  <Banner />
-  <p>this is my content</p>
-  <p>{name}</p>
-  <p>{age}</p>
-  <p>{person.name}</p>
-  <p>{person.age}</p>
-</section>);
+
+class App extends React.Component {
+  render(h) {
+    return (
+      <section>
+        <Banner />
+        <p>this is my content</p>
+        <p>{name}</p>
+        <p>{age}</p>
+        <p>{person.name}</p>
+        <p>{person.age}</p>
+      </section>
+    );
+  }
+}
 
 export default App;
